caf3ae417a0a2bb65a1d8dc9d4086527c99c89ba
ztYasmine <147100531+ztYasmine@users.noreply.github.com>
2023-12-09 22:40:38 -0500

    Update Kaggle House Pirces Data Analytics.R
    Adding column:SalePrice, GarageCond, PavedDrive, WoodDeckSF, OpenPorchSF, EnclosedPorch, X3SsnPorch, ScreenPorch, PoolArea

--- Kaggle House Pirces Data Analytics.R
+++ Kaggle House Pirces Data Analytics.R
@@ -45,3 +45,53 @@ library(tidyverse)
 summary(train)
 
 summary(lm(SalePrice ~ Exterior2nd + MasVnrType + MasVnrArea + ExterQual + ExterCond + Foundation + BsmtQual + BsmtCond, data = train))
+
+#Data Wrangling of "SalePrice","GarageCond", "PavedDrive", "WoodDeckSF", "OpenPorchSF", "EnclosedPorch", "X3SsnPorch", "ScreenPorch", "PoolArea"
+###Ziting###
+task_EDA <- train[, c("SalePrice","GarageCond", "PavedDrive", "WoodDeckSF", "OpenPorchSF", "EnclosedPorch", "X3SsnPorch", "ScreenPorch", "PoolArea")]
+train$GarageCond
+str(train)
+task_EDA$PavedDrive
+# Data cleaning
+# Example: Processing missing values, here fill NA as an example, the specific method depends on the data situation
+task_EDA <- task_EDA %>% 
+  mutate(
+    SalePrice = ifelse(is.na(SalePrice), 0, SalePrice),
+    GarageCond = ifelse(is.na(GarageCond), "None", GarageCond),
+    PavedDrive = ifelse(is.na(PavedDrive), "None", PavedDrive),
+    WoodDeckSF = ifelse(is.na(WoodDeckSF), 0, WoodDeckSF),
+    OpenPorchSF = ifelse(is.na(OpenPorchSF), 0, OpenPorchSF),
+    EnclosedPorch = ifelse(is.na(EnclosedPorch), 0, EnclosedPorch),
+    X3SsnPorch = ifelse(is.na(X3SsnPorch), 0, X3SsnPorch),
+    ScreenPorch = ifelse(is.na(ScreenPorch), 0, ScreenPorch),
+    PoolArea = ifelse(is.na(PoolArea), 0, PoolArea)
+  )
+
+#Check the cleaning data
+str(task_EDA)
+
+#According to the data attributes, Garagecond and PavedDrive two groups of character data are encoded and converted
+#Code from ChatGPT：https://chat.openai.com/share/3e284d2c-8236-48bd-a7e6-04db06b5970f
+#1.Process the Gargecond value: sort it first, then sort it
+
+#Create a binary feature with or without a garage
+task_EDA$HasGarage <- ifelse(task_EDA$GarageCond == "None", 0, 1)
+
+#Code situations with garages in order
+garageCondLevels <- setNames(c(5, 4, 3, 2, 1), c("Ex", "Gd", "TA", "Fa", "Po"))
+task_EDA$GarageCondNum <- garageCondLevels[task_EDA$GarageCond]
+
+#2.Encode and process PavedDrive data
+# Determine the coding logic for Y, P, and N
+# Use the lm() function to explore the effect of PavedDrive on SalePrice to determine the specific intervals of N, Y, and P values
+model_PD <- lm(SalePrice ~ PavedDrive, data = task_EDA)
+summary(model_PD)
+
+
+#Defined sequential mapping
+pavedDriveLevels <- setNames(c(3, 1, 0), c("Y", "P", "N"))
+
+#Apply 
+task_EDA$PavedDriveNum <- pavedDriveLevels[task_EDA$PavedDrive]
+
+Summary(lm(SalePrice ~ GarageCond + PavedDrive + WoodDeckSF + OpenPorchSF + EnclosedPorch + X3SsnPorch + ScreenPorch + PoolArea,data = task_EDA))
